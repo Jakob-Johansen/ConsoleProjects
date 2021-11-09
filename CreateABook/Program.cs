5db1782f300d56b17d32a8ac22360c5bbc2e9197
Jakob <Jakob@LAPTOP-E5LS5J1G>
2021-11-09 09:53:20 +0100

    Update "CreateABook".

--- CreateABook/Program.cs
+++ CreateABook/Program.cs
@@ -4,10 +4,11 @@ namespace CreateABook
 {
     class Program
     {
-        // I dette program kan du oprette bøger, redigere bøger og få dem vist i Konsol.
+        // In this program you can Create, Delete and show all books, using a database.
         static void Main(string[] args)
         {
-            // Skal kunne fortryde under create mode og måske redigere en bog.
+            // Skal kunne stoppe og undo under create mode og måske redigere en bog.
+            // Brug async til database kald.
 
             // Console.ReadKey(true).Key == ConsoleKey.Escape
 
